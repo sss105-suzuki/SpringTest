308e6080517c65fc477ad9d53dfc1b9733070efb
user.name <suzuki@soramasu.co.jp>
2015-07-03 14:14:57 +0900

    test

--- SpringTest/src/sample1/Main.java
+++ SpringTest/src/sample1/Main.java
@@ -1,13 +1,20 @@
 package sample1;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class Main {
 	public static void main(String[] args){
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("beans.xml"));
-		CallName cn = factory.getBean("callName",CallName.class);
-		cn.callName("go my own way!");
+		//インスタンス生成準備
+		DefaultListableBeanFactory  factory2 = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory2);
+		//beans.xml読み込み(クラスパスより上からは読み込めない？)
+		reader.loadBeanDefinitions((Resource)new ClassPathResource("./beans.xml"));
+		//インスタンス生成
+		CallName cN = factory2.getBean("callName", CallName.class);
+		//インスタンスメソッド実行
+		cN.callName("go my own way!");
 	}
 }
